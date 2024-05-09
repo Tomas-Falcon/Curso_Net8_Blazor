b8a9454b74816adc730ce5ccc9d815b38540f666
Tomas Falcon <tomasfalconblog@gmail.com>
2024-05-09 19:39:55 +0200

    Tarea POST Curso .NET 8, Finalizada

---
+++ PUT_Swagger_Petstore/PUT_Swagger_Petstore/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://petstore.swagger.io/v2/pet";
+
+        string jsonBody = @"{
+            ""id"": 0,
+            ""name"": ""doggie""
+        }";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                //si devuelve un 200, exitoso
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("La mascota fue creada exitosamente.");
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine("\nJSON de la respuesta:");
+                    Console.WriteLine(jsonResponse);
+                }
+                else
+                {
+                    Console.WriteLine($"Error al crear la mascota. Código de estado: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrió un error: {ex.Message}");
+            }
+        }
+    }
+}
